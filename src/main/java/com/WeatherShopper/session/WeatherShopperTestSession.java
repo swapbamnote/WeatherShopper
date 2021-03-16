47b0065db86558b1d97e42a52d559b4b7f9f23c1
sbamnote <sbamnote@carrentals.com>
2021-03-16 16:23:04 +0100

    call launchpage from session

--- src/main/java/com/WeatherShopper/session/WeatherShopperTestSession.java
+++ src/main/java/com/WeatherShopper/session/WeatherShopperTestSession.java
@@ -1,5 +1,17 @@
 package com.WeatherShopper.session;
 
+import org.testng.Reporter;
+
+import com.WeatherShopper.base.pages.WeatherShopperPage;
+import com.WeatherShopper.pages.application.LaunchPage;
+
 public class WeatherShopperTestSession {
 
+	public WeatherShopperPage init() {
+		if(Reporter.getCurrentTestResult().getTestContext().getAttribute("session") == null) {
+			Reporter.getCurrentTestResult().getTestContext().setAttribute("session", this);
+		}
+		
+		return new LaunchPage();
+	}
 }
--- src/test/java/com/WeatherShopper/ProductTypeTest.java
+++ src/test/java/com/WeatherShopper/ProductTypeTest.java
@@ -7,19 +7,27 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.Test;
 
+import com.WeatherShopper.base.pages.WeatherShopperPage;
 import com.WeatherShopper.listener.WeatherShopperEventListener;
+import com.WeatherShopper.session.WeatherShopperTestSession;
 
 public class ProductTypeTest {
 	
 	@Test
 	public void chooseProductType() {
-		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver/chromedriver");
+		
+		WeatherShopperTestSession session = new WeatherShopperTestSession();//new driver, new browser, onesessionpertest
+		WeatherShopperPage page = session.init();
+		page.openBrowser("Chrome");
+		
+		
+		/*System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver/chromedriver");
 		EventFiringWebDriver driver = new EventFiringWebDriver(new ChromeDriver());
 		driver.register(new WeatherShopperEventListener());
 		
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.get("https://weathershopper.pythonanywhere.com/");
+		driver.get("https://weathershopper.pythonanywhere.com/");*/
 	}
 
 }
